e683a969a6eae595d42b0a09ff7771e97f0917c3
hanske1967 <hanske1967@gmail.com>
2013-04-13 22:37:02 +0200

    EasyFit-62 Support special characters

---
+++ install/sql/update_2.3.2.sql
@@ -0,0 +1,19 @@
+use easyfit;
+
+update product set creationuser = 'INGE' where creationuser is null ;
+update product set creationdate = updatedate where creationdate is null ;
+
+update productcategory set creationuser = 'INGE' where creationuser is null ;
+update productcategory set creationdate = updatedate where creationdate is null ;
+
+update consumption set creationuser = updateuser where creationuser is null ;
+update consumption set creationdate = updatedate where creationdate is null ;
+
+update consumptiondetail set creationuser = updateuser where creationuser is null ;
+update consumptiondetail set creationdate = updatedate where creationdate is null ;
+
+update user set creationuser = 'HANS';
+
+update unit set creationuser = 'INGE';
+
+update unit set creationuser = 'INGE';
